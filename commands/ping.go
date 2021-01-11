8a1a0ffa219e2044714aaf96e4335f89f7166be2
MediaMagnet <nathanec@gmail.com>
2021-01-11 15:18:29 -0600

    ping works

--- commands/ping.go
+++ commands/ping.go
@@ -3,12 +3,15 @@ package commands
 import (
 	"Gemini/config"
 	"Gemini/lib"
+	"fmt"
 	"github.com/bwmarrin/discordgo"
 	"github.com/go-ping/ping"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 	"strings"
 )
+
+var pingMax []string
 
 func PingCommand(s *discordgo.Session, m *discordgo.MessageCreate) {
 	viper.SetConfigName("config")
@@ -30,60 +33,45 @@ func PingCommand(s *discordgo.Session, m *discordgo.MessageCreate) {
 		if err != nil {
 			log.Warnln(err)
 		}
-		disping, err := ping.NewPinger("www.discord.com")
-		if err != nil {
-			log.Errorf("b %v", err)
-		}
-		monping, err := ping.NewPinger("gemini-shard-00-02.hjehy.mongodb.net")
-		if err != nil {
-			log.Errorf("c %v", err)
-		}
-		gemping, err := ping.NewPinger("geminibot.xyz")
-		if err != nil {
-			log.Errorf("a %v", err)
-		}
 
-/*		msg1, _ := s.ChannelMessageSendEmbed(m.ChannelID, &discordgo.MessageEmbed{
+		msg1, _ := s.ChannelMessageSendEmbed(m.ChannelID, &discordgo.MessageEmbed{
+			Color:       0x00FF00,
 			Fields: []*discordgo.MessageEmbedField{
 				{Name: "Discord", Value: "Pinging..."},
 				{Name: "Mongo", Value: "Pinging..."},
 				{Name: "Other Half", Value: "Pinging..."},
 			},
-		})*/
-
-		disping.SetPrivileged(true)
-		monping.SetPrivileged(true)
-		gemping.SetPrivileged(true)
-
-		disping.Count = 3
-		monping.Count = 3
-		gemping.Count = 3
-
-		err = disping.Run()
-		if err != nil {
-			log.Errorf("d %v", err)
-		}
-		err = monping.Run()
-		if err != nil {
-			log.Errorf("e %v", err)
-		}
-		err = gemping.Run()
-		if err != nil {
-			log.Errorf("f %v", err)
-		}
-		// disStats := disping.Statistics()
-		// monStats := monping.Statistics()
-		// gemStats := gemping.Statistics()
-		log.Infof("a, %v", disping.Statistics())
-		log.Infof("b, %v", monping.Statistics())
-		log.Infof("c, %v", gemping.Statistics())
-
-/*		_, _ = s.ChannelMessageEditEmbed(m.ChannelID, msg1.ID, &discordgo.MessageEmbed{
-			Fields: []*discordgo.MessageEmbedField{
-				{Name: "Discord", Value: disStats.MaxRtt.String()},
-				{Name: "Mongo", Value: monStats.MaxRtt.String()},
-				{Name: "Other Half", Value: gemStats.MaxRtt.String()},
+			Footer: &discordgo.MessageEmbedFooter{
+				Text:    "Gemini: the other half of the battle.",
+				IconURL: "https://cdn.discordapp.com/avatars/783065070682243082/418be8dcc03596073565a8706ed519ec.png?size=16",
 			},
-		})*/
+		})
+
+		addrs := [2]string{"discord.com", "gemini-shard-00-02.hjehy.mongodb.net"}
+		for _, s := range addrs {
+			pinger, err := ping.NewPinger(s)
+			pinger.SetPrivileged(true)
+			if err != nil {
+				panic(err)
+			}
+			pinger.Count = 3
+			pinger.OnFinish = func(stats *ping.Statistics) {
+				fmt.Println(stats)
+				pingMax = append(pingMax, stats.AvgRtt.String())
+			}
+			_ = pinger.Run() // blocks until finished
+		}
+		_, _ = s.ChannelMessageEditEmbed(m.ChannelID, msg1.ID, &discordgo.MessageEmbed{
+			Color:       0x0047AB,
+			Fields: []*discordgo.MessageEmbedField{
+				{Name: "Discord", Value: pingMax[0]},
+				{Name: "Mongo", Value: pingMax[1]},
+				{Name: "Other Half", Value: "Watching the world go by"},
+			},
+			Footer: &discordgo.MessageEmbedFooter{
+				Text:    "Gemini: the other half of the battle.",
+				IconURL: "https://cdn.discordapp.com/avatars/783065070682243082/418be8dcc03596073565a8706ed519ec.png?size=16",
+			},
+		})
 	}
 }
